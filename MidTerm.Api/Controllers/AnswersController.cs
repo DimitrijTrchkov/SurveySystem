ebfa6c7ec60871a8734b1c67b725e95e45d847a8
DimitrijTrchkov <dimitrckov@hotmail.com>
2021-03-17 22:10:33 +0100

    AnswersController Implemented

--- MidTerm.Api/Controllers/AnswersController.cs
+++ MidTerm.Api/Controllers/AnswersController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MidTerm.Services.Abstractions;
+using MidTerm.Services.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,6 +17,42 @@ namespace MidTerm.Api.Controllers
         public AnswersController(IAnswersService service)
         {
             _service = service;
+        }
+
+        [HttpGet]
+        [Route("")]
+        public async Task<IActionResult> GetAllAnswers()
+        {
+            var response = await _service.GetAllAnswers();
+            return Ok(response);
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<IActionResult> GetAnswer([FromRoute] int id)
+        {
+            var response = await _service.GetAnswers(id);
+            return Ok(response);
+        }
+        [HttpPost]
+        [Route("")]
+        public async Task<IActionResult> CreateAnswers([FromBody] AnswersDTO request)
+        {
+            var response = await _service.CreateAnswers(request);
+            return Ok(response);
+        }
+        [HttpPatch]
+        [Route("{id}")]
+        public async Task<IActionResult> EditAnswer([FromRoute] int id, [FromBody] AnswersDTO request)
+        {
+            var response = await _service.EditAnswers(id, request);
+            return Ok(response);
+        }
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> DeleteAnswers([FromRoute] int id)
+        {
+            var response = await _service.DeleteAnswers(id);
+            return Ok(response);
         }
     }
 }
